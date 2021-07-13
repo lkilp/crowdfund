7f4ecee1629d0fb8e0a32b96cbf3333c22f06131
liis.kilp <kilp.liis@gmail.com>
2021-07-13 09:48:41 +0200

    fix routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,29 @@ import React, { useState } from 'react';
 import './Header.scss';
 
 const Header = () => {
-    const [ menuOpen, setMenuOpen ] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     }
-    return(
+    return (
         <div className="Header">
             <div className="filter">
                 <div className="navigation-bar">
-                <div className="logo"><a href='/'>crowdfund</a></div>
-                <nav>
-                    <a href="/">About</a>
-                    <a href="/">Discover</a>
-                    <a href="/">Get Started</a>
-                </nav>
-                <div className={"hamburger" + (menuOpen ? " close" : "")} onClick={() => toggleMenu()} ></div>
+                    <div className="logo"><a href='/'>crowdfund</a></div>
+                    <nav>
+                        <a href=".">About</a>
+                        <a href=".">Discover</a>
+                        <a href=".">Get Started</a>
+                    </nav>
+                    <div className={"hamburger" + (menuOpen ? " close" : "")} onClick={() => toggleMenu()} ></div>
                 </div>
-                { menuOpen && <div className="mobile-filter"/> }
-                { menuOpen && <div className="mobile-menu">
-                    <a href='/'>About</a>
-                    <a href='/'>Discover</a>
-                    <a href='/'>Get Sarted</a>
-                </div> }
+                {menuOpen && <div className="mobile-filter" />}
+                {menuOpen && <div className="mobile-menu">
+                    <a href='.'>About</a>
+                    <a href='.'>Discover</a>
+                    <a href='.'>Get Sarted</a>
+                </div>}
             </div>
         </div>
     )
